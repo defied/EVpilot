78ba5df491112b3e383bbab3710f561918e0bbde
defied <defied@deadplanet.net>
2017-05-01 11:24:33 -0700

    more genericizing

--- evelogger/run_split.sh
+++ evelogger/run_split.sh
@@ -5,6 +5,8 @@
 HIT=$(nslookup vkpilot.live-valkyrieapi.com | grep Address | grep -v '127.0' | cut -f 2 -d ' ')
 HIT=$(echo $HIT | sed -r 's/ /,/g')
 DIR='/home/defied/eveLogger/'
+HOST='10.0.51.254'
+
 #Route any traffic on port 443 to/from $HIT to port 8443.
 iptables -t nat -A PREROUTING -p tcp -d $HIT --dport 443 -j REDIRECT --to-ports 8443
 sleep 1
@@ -15,10 +17,10 @@ echo "Hit Enter to Continue"
 read
 
 # Split traffic going through port 8443 out to files in $DIR.
-sslsplit -D  -j /tmp/sslsplit/ -S ./ -k ca.key -c ca.crt https 10.0.51.254 8443 -S $DIR
+sslsplit -D  -j /tmp/sslsplit/ -S ./ -k ca.key -c ca.crt https $HOST 8443 -S $DIR
 
 # On teardown, remove redirect rule from iptables.
-iptables -t nat -D PREROUTING -p tcp -d 52.48.94.106,52.51.224.61,52.18.156.124 --dport 443 -j REDIRECT --to-ports 8443
+iptables -t nat -D PREROUTING -p tcp -d $HIT --dport 443 -j REDIRECT --to-ports 8443
 sleep 1
 echo "##################"
 echo $(iptables -t nat -L | grep '8443')
