a8f2f6181bdc11d880d087b2ac168872a676f96c
defied <defied@deadplanet.net>
2017-05-01 11:21:35 -0700

    clean up and genericize

--- evelogger/run_split.sh
+++ evelogger/run_split.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-iptables -t nat -A PREROUTING -p tcp -d 52.48.94.106,52.51.224.61,52.18.156.124 --dport 443 -j REDIRECT --to-ports 8443
+# Requires sslsplit utility.
+# Maybe one day you become python utility.
+
+HIT=$(nslookup vkpilot.live-valkyrieapi.com | grep Address | grep -v '127.0' | cut -f 2 -d ' ')
+HIT=$(echo $HIT | sed -r 's/ /,/g')
+DIR='/home/defied/eveLogger/'
+#Route any traffic on port 443 to/from $HIT to port 8443.
+iptables -t nat -A PREROUTING -p tcp -d $HIT --dport 443 -j REDIRECT --to-ports 8443
 sleep 1
 echo "##################"
 echo $(iptables -t nat -L | grep '8443')
 echo "##################"
 echo "Hit Enter to Continue"
 read
-#sslsplit -D -j /tmp/sslsplit/ -S ./ -k ca.key -c ca.crt ssl 10.0.51.254 8443 -L /root/evelogger/pipe
-sslsplit -D -j /tmp/sslsplit/ -S ./ -k ca.key -c ca.crt https 10.0.51.254 8443 -L /root/evelogger/pipe
+
+# Split traffic going through port 8443 out to files in $DIR.
+sslsplit -D  -j /tmp/sslsplit/ -S ./ -k ca.key -c ca.crt https 10.0.51.254 8443 -S $DIR
+
+# On teardown, remove redirect rule from iptables.
 iptables -t nat -D PREROUTING -p tcp -d 52.48.94.106,52.51.224.61,52.18.156.124 --dport 443 -j REDIRECT --to-ports 8443
 sleep 1
 echo "##################"
 echo $(iptables -t nat -L | grep '8443')
 echo "##################"
+
+# Bob's your uncle.
 exit 0
