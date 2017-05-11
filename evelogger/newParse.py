fc47b9c5646aa8f728567837360826f9aeb97506
defied <d.rhys-jones@temperednetworks.com>
2017-05-10 20:29:24 -0700

    1

--- evelogger/newParse.py
+++ evelogger/newParse.py
@@ -21,7 +21,7 @@
 #       Tab-delimited tree of information.
 #       Information levels?
 #       CSV output file (Tricky with subComponents)
-
+import argparse
 import json
 import re
 import csv
@@ -30,19 +30,49 @@ import glob
 import sys
 import time
 
-fileName = 'AIPilots.json'
+# Set option flags.
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--pilot', help='Bond data to specific pilot', required=False)
+parser.add_argument('-l', '--list', help='list objects and components', required=False)
+parser.add_argument('-o', '--object', help='Specify specific object to poll', default='')
+parser.add_argument('-d', '--directory', help='Directory to read', required=True)
+parser.add_argument('-v', '--hwVersion', help='Specify VMware Hardware Version. Default is 9.', default='9')
 
-fileIn = open("{}".format(fileName), 'r')
-fileRead = fileIn.read()
-fileIn.close()
-file_parsed = json.loads(fileRead)
-fi_data = file_parsed['aipilots'] #"{}".format(keyFlags)]
+args = parser.parse_args()
+statLog.debug(args)
 
-# open a file for writing
+def myprint(d):
+    ret = ''
+    for k, v in d.iteritems():
+        if isinstance(v, dict):
+            ret = ret + "{0}".format(k)#d.iterkeys().next())
+            myprint(v)
+        else:
+            ret = ret + "    {0} : {1}".format(k, v)
 
-file_data = open('/tmp/fileData.csv', 'w')
+def json_list(directory):
+    for fileName in glob.glob(logPath+"/*.log"):
+        fileIn = open("{}".format(fileName), 'r')
+        fileRead = fileIn.read()
+        fileIn.close()
+        file_parsed = json.loads(fileRead)
+        objectOut = myprint(file_parsed)
+
+def json_parser(objects, lists, file):
+    fileIn = open("{}".format(fileName), 'r')
+    fileRead = fileIn.read()
+    fileIn.close()
+    file_parsed = json.loads(fileRead)
+    fi_data = file_parsed['aipilots'] #"{}".format(keyFlags)]
+
+
+if args.list:
+    json_list(args.directory)
+
 
 def cvs_write()
+    # open a file for writing
+    file_data = open('/tmp/fileData.csv', 'w')
     # create the csv writer object
     csvwriter = csv.writer(file_data)
     count = 0
