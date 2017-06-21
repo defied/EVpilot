f082c0e29567b2a20c801a79c074d808624eba79
root <root@deathByDoornails.deadplanet.net>
2017-06-21 12:06:22 -0700

    using post-parsed files.json to perform web calls. Should probably delete file afterward as the links time out after a short period

--- evelogger/get_valkyrie_aws.sh
+++ evelogger/get_valkyrie_aws.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-LogDir=/home/defied/eveLogger
+LogDir=/var/www/html/evevalkyrie/livepilot/files.json
 DIR=/var/www/html/evevalkyrie/ValkyrieLIVE/
 
 
-for i in $(cat $LogDir/*log | grep '"uri"' | grep -i amazon | cut -f 4 -d '"')
+for i in $(cat $LogDir | grep '"uri"' | grep -i amazon | cut -f 4 -d '"')
 do
     tmpDir=$(echo $i | cut -f 4 -d '"' | cut -f 1 -d '?' | cut -f 6 -d '/')
     mkdir -p $DIR$tmpDir
 done
 
-for i in $(cat $LogDir/*log | grep '"uri"' | grep -i amazon | cut -f 4 -d '"')
+for i in $(cat $LogDir | grep '"uri"' | grep -i amazon | cut -f 4 -d '"')
 do
     tmpDir=$(echo $i | cut -f 4 -d '"' | cut -f 1 -d '?' | cut -f 6,7 -d '/')
     curl $i > $DIR$tmpDir
